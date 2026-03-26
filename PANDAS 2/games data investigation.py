c64469a47277372d2f921d8515bb0ddbd294826f
Matthew <109.2022.84928@student.donegaletb.ie>
2026-03-26 10:30:40 +0000

    Update games data investigation.py

--- PANDAS 2/games data investigation.py
+++ PANDAS 2/games data investigation.py
@@ -7,7 +7,7 @@ games = pd.read_csv("steam_games_2026.csv") # read csv file
 games.columns = games.columns.str.strip().str.lower().str.replace(" ", "_")
 # clean columns to lowercase and replace space with underscore
 
-games = games.sample(101)
+games = games.sample(101) # decrease sample size because its overwhelming
 
 print("Columns:", list(games.columns))
 # prints list of columns
@@ -15,9 +15,10 @@ print("Columns:", list(games.columns))
 games = games.drop_duplicates()
 games = games.dropna() # cleaning. dropna() removes NaN (Not a Number) values, drop_duplicates removes duplicate entries
 
-print("\nSelected columns from games:")
-
 games = games.drop(columns = ["appid", "primary_genre", "all_tags", "price_usd", "discount_pct", "review_score_pct", "total_reviews", "steam_deck_status", "24h_peak_players"])
+# drops unnecessary columns
 
+top_games = games.sort_values(by="estimated_owners", ascending = False)
+print("\n Sorted top games by owner count:")
 
-print(games)
+print(top_games)
